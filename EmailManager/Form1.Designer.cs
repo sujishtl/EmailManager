cbb2625c5c6972c4b2427842db3e0ec837a05da8
Sujish Lakshmanan <Sujishtl@gmail.com>
2020-07-20 19:12:22 +1000

    Working copy

--- EmailManager/Form1.Designer.cs
+++ EmailManager/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
@@ -36,25 +38,49 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(36, 46);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.Size = new System.Drawing.Size(455, 95);
             this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GetItems";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(-1, 245);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(35, 13);
+            this.statusLabel.TabIndex = 2;
+            this.statusLabel.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(595, 264);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label statusLabel;
     }
 }
 
--- EmailManager/Form1.cs
+++ EmailManager/Form1.cs
@@ -8,10 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Office.Interop.Outlook;
+//For Outlook 2010, you'll need to add reference to Microsoft.Office.Interop.Outlook version 14.0.0.0 else go with the latest
+using System.IO;
 namespace EmailManager
 {
     public partial class Form1 : Form
     {
+        string path = @"D:\log.txt";
         public Form1()
         {
             InitializeComponent();
@@ -19,20 +22,107 @@ namespace EmailManager
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            var outlookApplication = new Microsoft.Office.Interop.Outlook.Application();
-            var outlookNameSpace = outlookApplication.GetNamespace("MAPI");
-            var inboxFolder = outlookNameSpace.GetDefaultFolder(OlDefaultFolders.olFolderInbox).Folders["Optus"]; ;
-            var mailItems = inboxFolder.Items;
 
-            foreach( MailItem item in mailItems)
+
+        }
+
+        private List<string> ReadMail()
+        {
+            List<string> mailList = new List<string>();
+            try
             {
-                if(item.SentOn>DateTime.Now.AddMinutes(-1200))
+                var outlookApplication = new Microsoft.Office.Interop.Outlook.Application();
+                var outlookNameSpace = outlookApplication.GetNamespace("MAPI");
+                //  var inboxFolder = outlookNameSpace.GetDefaultFolder(OlDefaultFolders.olFolderInbox).Folders["Optus"];
+                var inboxFolder = outlookNameSpace.GetDefaultFolder(OlDefaultFolders.olFolderInbox);
+                Log("FolderName:" + inboxFolder.FullFolderPath.ToString());
+                var mailItems = inboxFolder.Items;
+                int count = mailItems.Count;//3122
+                int index = 0;
+                List<string> objectTypes = new List<string>();
+
+                //foreach (Object o in mailItems)
+                //{
+                //    if (!objectTypes.Contains(o.GetType().ToString()))
+                //    {
+                //        objectTypes.Add(o.GetType().ToString());
+                //        Log(o.GetType().ToString());
+                //    }
+                //}
+
+
+                //foreach ( Object o in mailItems)
+                //{
+                //    //try
+                //    //{
+                //    //    var item = (MailItem)o;
+                //    //}
+                //    //catch(System.Exception ex)
+                //    //{
+
+                //    //}
+
+
+                //  if(typeof(MailItem)==o.GetType())
+                //    {
+
+                //    }
+                //  else
+                //    {
+                //        mailItems.Remove(index);
+                //       Log("Removed Line:" + index);
+                //    }
+                //    index = index + 1;
+                //}
+
+                //  foreach (MailItem item in mailItems)
+                for (int x = 1; x < count; x++)
                 {
-                    listBox1.Items.Add(item.Subject);
-                    
+
+                    //if (mailItems[x].SentOn > DateTime.Now.AddMinutes(-9000))
+                    //{
+
+                        mailList.Add(mailItems[x].SenderEmailAddress + "," + mailItems[x].SentOn + "," + mailItems[x].Subject);
+
+                    //}
                 }
+
+
+            }
+            catch (System.Exception ex)
+            {
+                statusLabel.Text = "Exception Occured" + ex.Message;
+                Log(ex.Message);
             }
 
+            statusLabel.Text = "Getting items completed";
+            return mailList;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var csvPath = @"D:\csvMails"+DateTime.Now.ToString("ddMMyyHHmmss")+".csv";
+            var itemsList = ReadMail();
+            using (var file = File.CreateText(csvPath))
+            {
+                foreach (string item in itemsList)
+                {
+                    listBox1.Items.Add(item);
+                    file.WriteLine(item);
+                }
+            }
+        }
+
+        public void Log(string content)
+        {
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+
+                File.WriteAllText(path, content + Environment.NewLine);
+            }
+            else
+                File.AppendAllText(path, content + Environment.NewLine);
         }
     }
 }
